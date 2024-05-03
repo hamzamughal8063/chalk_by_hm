d722cc47742cde675555f51eca521ec5dd2941ae
Hamza Mughal <163909829+hamzamughal8063@users.noreply.github.com>
2024-05-04 00:55:09 +0500

    Add files via upload
    chalk_by_hm

---
+++ index.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+import chalk from 'chalk';
+let name;
+console.log(chalk.greenBright.bold.underline("Hamza Mughal"));
---
+++ index.ts
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+import chalk from 'chalk';
+
+
+let name: "Hamza Mughal";
+console.log(chalk.greenBright.bold.underline("Hamza Mughal"));
